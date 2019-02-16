817ddfcde82199a73690d71f310adadf957eaf3d
antohaUa <antoha.ua@gmail.com>
2019-02-16 22:54:02 +0200

    Added flow schema to examples

--- examples/write_virtual_pin.py
+++ examples/write_virtual_pin.py
@@ -1,26 +1,55 @@
 """
-Blynk is a platform with iOS and Android apps to control
-Arduino, Raspberry Pi and the likes over the Internet.
-You can easily build graphic interfaces for all your
-projects by simply dragging and dropping widgets.
-  Downloads, docs, tutorials: http://www.blynk.cc
-  Sketch generator:           http://examples.blynk.cc
-  Blynk community:            http://community.blynk.cc
-  Social networks:            http://www.fb.com/blynkapp
-                              http://twitter.com/blynk_app
+[WRITE VIRTUAL PIN EXAMPLE] ========================================================================
 
-This example shows how to capture write events when some data was changed by widget.
+Environment prepare:
 In your Blynk App project:
   - add "Slider" widget,
   - bind it to Virtual Pin V4,
   - set values range 0-255
   - add "LED" widget and assign Virtual Pin V4 to it
   - Run the App (green triangle in the upper right corner).
+  - define your auth token for current example and run it
 
 
 This started program will periodically call and execute event handler "write_virtual_pin_handler".
 In app you can move slider that will cause LED brightness change and will send virtual write event
 to current running example. Handler will print pin number and it's updated value.
+
+Schema:
+====================================================================================================
+          +-----------+                        +--------------+                    +--------------+
+          |           |                        |              |                    |              |
+          | blynk lib |                        | blynk server |                    |  blynk app   |
+          |           |                        |  virtual pin |                    |              |
+          |           |                        |              |                    |              |
+          +-----+-----+                        +------+-------+                    +-------+------+
+                |                                     |                                    |
+                |                                     |  write event from "Slider" widget  |
+                |                                     |                                    |
+                |                                     +<-----------------------------------+
+                |                                     |                                    |
+                |                                     |                                    |
+                |                                     |                                    |
+                |                                     |                                    |
+ event handler  |   write event to hw from server     |                                    |
+(user function) |                                     |                                    |
+     +-----------<------------------------------------+                                    |
+     |          |                                     |                                    |
+     |          |                                     |                                    |
+     +--------->+                                     |                                    |
+                |                                     |                                    |
+                |                                     |                                    |
+                |                                     |                                    |
+                +                                     +                                    +
+====================================================================================================
+Additional blynk info you can find by examining such resources:
+
+    Downloads, docs, tutorials:     http://www.blynk.cc
+    Sketch generator:               http://examples.blynk.cc
+    Blynk community:                http://community.blynk.cc
+    Social networks:                http://www.fb.com/blynkapp
+                                    http://twitter.com/blynk_app
+====================================================================================================
 """
 
 import BlynkLib
