e367959ed0b4f0215e83a1334aacc8f2165bf4db
Blynk IoT platform <pavel@blynk.cc>
2019-03-24 11:34:18 -0400

    Update 01_touch_button.py

--- examples/esp32/01_touch_button.py
+++ examples/esp32/01_touch_button.py
@@ -27,7 +27,7 @@ operation updates widget value. In app you can see touch event updated info. Ex 
 =====================================================================================================================
 Additional info about blynk you can find by examining such resources:
 
-    Downloads, docs, tutorials:     http://www.blynk.cc
+    Downloads, docs, tutorials:     https://blynk.io
     Sketch generator:               http://examples.blynk.cc
     Blynk community:                http://community.blynk.cc
     Social networks:                http://www.fb.com/blynkapp
